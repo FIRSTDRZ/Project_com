cbd06221b00861670c352459d99cc0d87c8d2a7f
Yew <yewit247@gmail.com>
2025-02-10 14:35:52 +0700

    update

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+if (choice == 3 ) {
+    //     cout<<"=========================="<<endl;
+    //     cout<<"Here are our memberships"<<endl;
+    //     cout<<"New Membership(1)"<<endl;
+    //     cout<<"Log in Membership(2)"<<endl;
+    //     cout<<"Edit Membership(3)"<<endl;
+    //     cout<<"Please choose the service you want to use: ";
+    //     int choice_member;
+    //     cin>>choice_member;
+        
+    // }

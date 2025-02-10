d00c5d6d4f03d956555a048e5e2183b40cbb63e7
Yew <yewit247@gmail.com>
2025-02-10 13:25:07 +0700

    test

--- first.cpp
+++ first.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello First C++ Program!" << std::endl;
+    std::cout << "Hello First C++ Program!" << std::endl;
+    std::cout << "cpe x ai" << std::endl;
     return 0;
 }

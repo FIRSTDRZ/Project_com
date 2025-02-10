4b6fe06e157a4382f440e24a22e031457f456c79
Yew <yewit247@gmail.com>
2025-02-10 14:04:08 +0700

    Basic UI

--- first.cpp
+++ first.cpp
@@ -1,8 +1,25 @@
-#include     <iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello First C++ Program!" << std::endl;
-    std::cout << "Hello First C++ Program!" << std::endl;
-    std::cout << "cpe x ai" << std::endl;
+    cout<<"=========================="<<endl;
+    cout<<"Hello Welcome to Jeng Karoke"<<endl;
+    cout<<"Here are our services"<<endl;
+    cout<<"=========================="<<endl;
+    cout<<"Booking(1)"<<endl;
+    cout<<"Type of Room(2)"<<endl;
+    cout<<"Memberships(3)"<<endl;
+    cout<<"Show Queue(4)"<<endl;
+    cout<<"Cancel Booking(5)"<<endl;
+    cout<<"Log out(6)"<<endl;
+    cout<<"=========================="<<endl;
+    cout<<"Please choose the service you want to use: ";
+    
+    int choice;
+    cin>>choice;    
+    
     return 0;
 }
